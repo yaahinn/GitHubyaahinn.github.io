3de60108af472429d8781d7247d7a9ea80555a18
yaa <m0a20024fc@edu.teu.ac.jp>
2022-10-19 18:37:35 +0900

    first commmiting

--- function-test.js
+++ function-test.js
@@ -1,3 +1,6 @@
+let a=10;
+let b=22
+
 function logHello() {
   console.log("Hello");
 }
@@ -6,6 +9,6 @@ function logmath(){
   return 2021*1010;
 }
 function logmath2(a,b){
-  return a+b;
+  return a*b;
 }
 
