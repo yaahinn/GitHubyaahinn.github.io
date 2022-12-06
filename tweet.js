bcc60723931465fdde8f251994da95959ff09514
yaa <m0a20024fc@edu.teu.ac.jp>
2022-12-06 23:23:29 +0900

    first commmiting

---
+++ tweet.js
@@ -0,0 +1,18 @@
+let tweet=document.querySelector("#tweets");
+
+
+function tweetALL(){
+  let
+      }
+
+function tweetsabu(){
+  let
+      }
+
+function tweetziro(){
+  let
+      }
+
+function tweettaro(){
+  let
+      }
